7e6732026535bc388bc523b48e712b4d194c2878
Sanando Chanda <sanandochanda@yahoo.com>
2024-12-04 16:25:00 +0530

    Registration changes

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ app.use((req, res, next) => {
 });
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    // origin: "https://microblog-tawny.vercel.app",
+    // origin: "http://localhost:5173",
+    origin: "https://microblog-tawny.vercel.app",
     credentials: true,
   })
 );
